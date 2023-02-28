20a4125598c80ddb6aa2dacdd6117495d13b4545
jpagett <pagett.jared@gmail.com>
2023-02-28 12:47:11 -0800

    gaussian beams

--- kamo/gaussian_beam/gaussian.py
+++ kamo/gaussian_beam/gaussian.py
@@ -1,16 +1,25 @@
 import numpy as np
 
 class GaussianBeam():
-    def __init__(self,power=0.,waist=0.,wavelength=0.,n_medium=1.):
-        self.power = power
+    def __init__(self,waist=0.,wavelength=0.,n_medium=1.):
         self.waist = waist
         self.wavelength = wavelength
         self.n_medium = n_medium
+        self.power = power
         self.rayleigh_range = np.pi * self.waist^2 * self.n_medium / self.wavelength
         self.divergence_angle = self.wavelength / np.pi / self.n_medium / self.waist
         
-    def beam_radius(self,z=0.):
+    def beam_radius(self,z):
         return self.waist * np.sqrt( 1 + (z / self.rayleigh_range)**2 )
     
-    def peak_intensity(self,z=0.):
-        return 2 * self.power / np.pi / self.beam_radius(z)
+    def peak_intensity(self,power,r=0.,z=0.):
+        '''
+        Returns the intensity of the gaussian beam at (r,z)
+
+        Parameters
+        ----------
+
+        '''
+        w0 = self.waist
+        wz = self.beam_radius(z)
+        return 2 * power / np.pi / wz**2 * np.exp(-2 * r**2 / wz )
