e65f2a8383869be2f481e84808f1e537b9d14270
jpagett <pagett.jared@gmail.com>
2023-03-01 16:43:26 -0800

    rewrap, add spot size code

--- kamo/gaussian_beam/thin_lens_gaussian.py
+++ kamo/gaussian_beam/thin_lens_gaussian.py
@@ -54,7 +54,8 @@ class Objective():
     focal_length: float
         The effective focal length (in m) of the objective (EFL)
     entrance_aperture_diameter
-        The entrance pupil diameter at the back aperture of the objective (EP = 2*NA*EFL)
+        The entrance pupil diameter at the back aperture of the objective (EP =
+        2*NA*EFL)
     '''
 
     def __init__(self, NA, working_distance, focal_length):
@@ -63,23 +64,42 @@ class Objective():
         self.focal_length = focal_length
         self.entrance_aperture_diameter = 2 * self.NA * self.focal_length
 
-    def input_beam_from_output_waist(self,output_waist,wavelength):
-        '''
-        Returns the required input gaussian beam for a given output beam waist,
-        assuming operation away from the limits of the objective.
+    # def input_beam_from_output_waist(self,output_waist,wavelength): '''
+    #     Returns the required input gaussian beam for a given output beam
+    #     waist, assuming operation away from the limits of the objective.
 
-        Parameters
-        ----------
-        output_waist: float
-            The 1/e^2 spot waist at the working distance from the objective
+    #     Parameters
+    #     ----------
+    #     output_waist: float The 1/e^2 spot waist at the working distance from
+    #         the objective
 
-        Outputs
-        -------
-        GaussianBeam
-        '''
+    #     Outputs
+    #     -------
+    #     GaussianBeam '''
 
-        output_beam = GaussianBeam(waist=output_waist, wavelength=wavelength)
-        eff_lens = ThinLensGaussian(self.focal_length)
-        input_beam = eff_lens.beam_after_lens(output_beam)
-        return input_beam
-        
+    #     output_beam = GaussianBeam(waist=output_waist, wavelength=wavelength)
+    #     eff_lens = ThinLensGaussian(self.focal_length) input_beam =
+    #     eff_lens.beam_after_lens(output_beam,z_input_waist_to_lens=self.focal_length)
+    #     return input_beam
+
+    def spot_waist_from_input_beam(self, input_beam):
+        beam_diameter = input_beam.waist * 2
+        wavelength = input_beam.wavelength
+        spot_waist = 2 * wavelength * self.focal_length / np.pi / beam_diameter
+    
+        if beam_diameter > self.entrance_aperture_diameter:
+                print(f"Input beam diameter ({beam_diameter}) is larger than the "+
+                    "entrance pupil of the objective ({self.entrance_aperture_diameter})")
+                
+        return spot_waist
+    
+    def input_waist_from_spot_beam(self, spot_beam):
+        spot_waist = spot_beam.waist * 2
+        wavelength = spot_beam.wavelength
+        input_waist = 2 * wavelength * self.focal_length / np.pi / spot_waist
+
+        if 2*input_waist > self.entrance_aperture_diameter:
+            print(f"Required input beam diameter ({2*input_waist}) is larger than the "+
+                  "entrance pupil of the objective ({self.entrance_aperture_diameter})")
+
+        return input_waist
