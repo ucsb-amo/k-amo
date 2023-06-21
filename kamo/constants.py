1215a2a84a378913668db0d573aea8e652e10591
jpagett <pagett.jared@gmail.com>
2023-06-20 22:01:03 -0700

    add bohr radius, polarizability conversion

--- kamo/constants.py
+++ kamo/constants.py
@@ -7,9 +7,12 @@ atom_K39 = arc.Potassium39()
 c = scon.c
 kB = scon.k
 h = scon.h
+a0 = scon.physical_constants['Bohr radius'][0]
 hbar = h / 2 / np.pi
 
 m_K = atom_K39.mass
 m_e = scon.m_e
 epsilon0 = scon.epsilon_0
+
+convert_polarizability_au_to_SI = 4 * np.pi * epsilon0 * a0**3
 
