919cf3be31151c942dd22831a7aeea7d0883fecb
bowjun <66513176+bowjun@users.noreply.github.com>
2023-06-22 17:10:54 -0700

    Update parse_portal_data.py

--- kamo/light_shift/parse_portal_data.py
+++ kamo/light_shift/parse_portal_data.py
@@ -72,38 +72,92 @@ def quantum_numbers_to_state_label(n,l,j):
 
     return f"{n}{Lstr}{jStr}"
 
-def reduced_dipole_matrix_element(n1,l1,j1,n2,l2,j2,portal_data:pd.DataFrame = None):
+def fraction_str_to_decimal(frac_str):
+    num,den = frac_str.split('/')
+    return float(num)/float(den)
+
+def state_label_to_quantum_numbers(state_str):
+
+    delims = ["s","p","d","f"]
+
+    Lstr = ""
+    for d in delims:
+        st_split = state_str.split(d)
+        if len(st_split) > 1:
+            Lstr = d
+            break
+
+    n = int(st_split[0])
+    j = fraction_str_to_decimal(st_split[-1])
+    
+    if Lstr == "s": l = 0
+    elif Lstr == "p": l = 1
+    elif Lstr == "d": l = 2
+    elif Lstr == "f": l = 3
+    else: 
+        print(Lstr)
+        raise ValueError("The state label string did not contain s, p, d, or f.")
+    
+    return n, l, j
+
+def reduced_dipole_matrix_element_table(n,l,j,portal_data:pd.DataFrame = None):
     """Returns the reduced dipole matrix element and energy for the specified transition.
 
     Args:
-        n1 (int): The n quantum number for the initial state.
-        l1 (int): The l quantum number for the initial state.
-        j1 (float): The j quantum number for the initial state.
-        n2 (int): The n quantum number for the final state.
-        l2 (int): The l quantum number for the final state.
-        j2 (float): The j quantum number for the final state.
+        n (int): The n quantum number for the initial state.
+        l (int): The l quantum number for the initial state.
+        j (float): The j quantum number for the initial state.
         portal_data (pd.DataFrame, optional): If the matrix element DataFrame
         has already been loaded, provide it here. If unspecified, loads from
         file. Defaults to None.
 
     Returns:
-        tuple: The dipole matrix element (in SI units), and the signed
-        transition energy (in J). Transition energy is defined as E_final -
-        E_initial.
+        pd.DataFrame: a dataframe containing the information about transitions
+        from the specified initial state.
+        np.ndarray: an ndarray containing the state-labeling string 
     """    
     if portal_data == None:
         portal_data = load_portal_data()
     elif not isinstance(portal_data,pd.DataFrame):
         raise ValueError("portal_data must be a pandas.DataFrame.")
     
-    state_i = quantum_numbers_to_state_label(n1,l1,j1)
-    state_f = quantum_numbers_to_state_label(n2,l2,j2)
+    state_i = quantum_numbers_to_state_label(n,l,j)
 
     elems_from_i = portal_data.loc[ portal_data['Initial'] == state_i ]
     if elems_from_i.empty:
         raise ValueError(f"No matrix element found from state {state_i}. Check the portal_data object.")
+    
+    allowed_final_states = elems_from_i['Final'].values
 
-    elem_i_to_f = elems_from_i.loc[ elems_from_i['Final'] == state_f ]
+    return elems_from_i, allowed_final_states
+
+def matrix_element_from_transition_table(nf,lf,jf,matrix_element_table=None):
+    """_summary_
+
+    Args:
+        nf (int): The final n quantum number
+        lf (int): The final l quantum number
+        jf (float): The final j quantum number
+        matrix_element_table (pd.DataFrame): The dataframe that
+        contains all the rows corresponding to transitions from a desired
+        initial state. 
+
+    Returns:
+        float: The reduced electric dipole matrix element for the transition
+        from the initial state to the final state in atomic units.
+        float: The signed transition energy in Joules, defined as E_f - E_i.
+    """
+    
+    state_i = matrix_element_table['Initial'].values[0]
+    ni,li,ji = state_label_to_quantum_numbers("4s1/2")
+
+    atom = arc.Potassium39()
+    energy_i = atom.getEnergy(ni,li,ji)
+    energy_f = atom.getEnergy(nf,lf,jf)
+
+    state_f = quantum_numbers_to_state_label(nf,lf,jf)
+
+    elem_i_to_f = matrix_element_table.loc[ matrix_element_table['Final'] == state_f ]
     if elem_i_to_f.empty:
         raise ValueError(f"No matrix element found from initial state ({state_i}) to {state_f}. Check the portal_data object.")
     
@@ -113,8 +167,8 @@ def reduced_dipole_matrix_element(n1,l1,j1,n2,l2,j2,portal_data:pd.DataFrame = N
     transition_energy_J = c.h * c.c / transition_wavelength_m
 
     # flip the sign on the transition energy if E_i > E_f
-    atom = arc.Potassium39()
-    if atom.getEnergy(n1,l1,j1) > atom.getEnergy(n2,l2,j2):
+    
+    if energy_i > energy_f:
         transition_energy_J = -1 * transition_energy_J
 
     return matrix_element_au, transition_energy_J
