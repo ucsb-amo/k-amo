a181c20e70ae29eee065e8467cadb4c831ab16ef
jpagett <pagett.jared@gmail.com>
2023-06-27 09:20:29 -0700

    include power in gaussian

--- kamo/gaussian_beam/gaussian.py
+++ kamo/gaussian_beam/gaussian.py
@@ -10,21 +10,36 @@ class GaussianBeam():
         Waist (in m)
     wavelength: float
         wavelength (in m)
+    power: float
+        power (in W)
     n_medium: float
         The optical index of the medium
-    divergence_angle: float
+
+    I0: float
+        The peak intensity (in W/m^2)
+    w0: float
+        An alias for the beam waist
+    z0: float
+        An alias for the rayleigh range.
 
     Methods
     -------
     beam_radius
-    peak_intensity
+    intensity
     '''
-    def __init__(self,waist,wavelength,n_medium=1.):
+    def __init__(self,waist,wavelength,power=0.,n_medium=1.):
         self.waist = waist
         self.wavelength = wavelength
         self.n_medium = n_medium
         self.rayleigh_range = np.pi * self.waist**2 * self.n_medium / self.wavelength
         self.divergence_angle = self.wavelength / np.pi / self.n_medium / self.waist
+        self.power = power
+        self.peak_intensity = self.intensity()
+
+        # aliases for commonly used parameters
+        self.I0 = self.peak_intensity
+        self.w0 = self.waist
+        self.z0 = self.rayleigh_range
         
     def beam_radius(self,z):
         '''
@@ -41,20 +56,22 @@ class GaussianBeam():
         '''
         return self.waist * np.sqrt( 1 + (z / self.rayleigh_range)**2 )
     
-    def intensity(self,power,r=0.,z=0.):
+    def intensity(self,power=-0.1,r=0.,z=0.):
         '''
         Returns the intensity of the gaussian beam at (r,z)
 
         Parameters
         ----------
         power: float
-            The power (in Watts) in the beam
+            The power (in Watts) in the beam (default = -0.1, uses power =
+            self.power)
         r: float
             The radial position (in m) from the beam axis (default = 0.)
-        z: float (default z=0.)
+        z: float
             The axial position (in m) from the beam waist (default = 0.)
         '''
-        w0 = self.waist
+        if power == -0.1:
+            power = self.power
         wz = self.beam_radius(z)
         return 2 * power / np.pi / wz**2 * np.exp(-2 * r**2 / wz )
     
